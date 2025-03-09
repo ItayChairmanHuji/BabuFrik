21ecbabce57f11b01d36d0e6739e9a43d28dcec9
Itay <test@test.com>
2025-03-09 15:25:42 +0200

    Bye bye node

--- src/statistics/runtime_statistics_analyzer.py
+++ src/statistics/runtime_statistics_analyzer.py
@@ -3,6 +3,10 @@ from src.utils.report import Report
 
 
 class RuntimeStatisticsAnalyzer(StatisticsAnalyzer):
+    @staticmethod
+    def mandatory_fields() -> list[str]:
+        return ["x_axis", "x_axis_label"]
+
     def analyze_statistics(self, reports: dict[str, list[Report]]) -> None:
         for service_name, reports in reports.items():
             runtimes = [report.end_time - report.start_time for report in reports]
