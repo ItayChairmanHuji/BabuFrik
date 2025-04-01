a56751b7dbf7ac6da9062f2fff3dcbdfc4c3c983
Itay <test@test.com>
2025-04-01 15:44:09 +0300

    log

--- src/repairers/ilp_repairer_base.py
+++ src/repairers/ilp_repairer_base.py
@@ -30,9 +30,8 @@ class ILPRepairerBase(Service, ABC):
         fds = load_fds(self.fds_file_path)
         marginals: Marginals = self.__load_marginals()
         self.config["marginals_error_margins_file_path"] = self.marginals_errors_margins_file_path
-        return self.__repair_data(data, fds, marginals) if not is_common_left_hand_side(fds) else (
-            pd.concat([self.__repair_data(block.reset_index().drop(columns=["index"]), fds, marginals)
-                       for _, block in data.groupby(fds[0].source)]))
+        return self.__repair_data(data, fds, marginals) if \
+            not is_common_left_hand_side(fds) else self.__common_left_hand_side_optimization
 
     def __load_marginals(self) -> Marginals:
         return object_loader.load(self.extra_data["marginals_file_path"]) \
@@ -54,3 +53,8 @@ class ILPRepairerBase(Service, ABC):
         ilp = self.ilp_type(data, fds, self.config, marginals)
         ilp.solve()
         return self.__get_feasible_solution(data, ilp) if ilp.did_succeed else self.__get_infeasible_solution(data)
+
+    def __common_left_hand_side_optimization(self, data: DataFrame,
+                                             fds: list[FunctionalDependency], marginals: Marginals) -> DataFrame:
+        print(len(data.groupby(fds[0].source)))
+        return pd.concat([self.__repair_data(block.reset_index().drop(columns=["index"]), fds, marginals) for _, block in data.groupby(fds[0].source)])
