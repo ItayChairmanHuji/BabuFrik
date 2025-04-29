b0182e4c1f77fcc69942b790d1303098cd89a4f9
Itay <test@test.com>
2025-04-29 16:32:54 +0300

    No need for so many GPUs

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=24:00:00
 #SBATCH --mem=100G
-#SBATCH --gres=gpu:3
+#SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=6
 #SBATCH --output=output_%j.log
 #SBATCH --error=error_%j.log
