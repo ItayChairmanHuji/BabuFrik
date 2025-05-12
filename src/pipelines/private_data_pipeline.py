7488cc5efb4b8b9d709461c0fc544bf817b96e8c
Itay <test@test.com>
2025-05-12 20:45:44 +0300

    Estetics

--- src/pipelines/private_data_pipeline.py
+++ src/pipelines/private_data_pipeline.py
@@ -1,4 +1,5 @@
 import ray
+
 from src.action import Action
 from src.pipelines.pipeline import Pipeline
 from src.task import Task
@@ -8,10 +9,9 @@ class PrivateDataPipeline(Pipeline):
     def run(self) -> None:
         results = []
         for private_data_size in self.config.private_data_size:
-            marginals_task = self.clean_data.remote(self,
-                Task(data=self.data, private_data_size=private_data_size,
-                     synthetic_data_size=self.config.synthetic_data_size,
-                     fds=self.fds, action=Action.CLEANING))
+            marginals_task = self.clean_data.remote(self,  Task(data=self.data, private_data_size=private_data_size,
+                                                               synthetic_data_size=self.config.synthetic_data_size,
+                                                               fds=self.fds, action=Action.CLEANING))
             synthesizing_task = self.get_marginals.remote(self, marginals_task)
             for _ in range(self.config.generations_repeats):
                 results.append(self.generate_synthetic_data.remote(self, synthesizing_task))
--- src/pipelines/synthetic_data_pipeline.py
+++ src/pipelines/synthetic_data_pipeline.py
@@ -1,5 +1,3 @@
-from dataclasses import replace
-
 from src.action import Action
 from src.pipelines.pipeline import Pipeline
 from src.task import Task
@@ -9,8 +7,8 @@ class SyntheticDataPipeline(Pipeline):
     def run(self) -> None:
         results = []
         for synthetic_data_size in self.config.synthetic_data_size:
-            clean_task = Task(data=self.data, private_data_size=self.config.private_data_size, 
-                synthetic_data_size=synthetic_data_size, fds=self.fds, action=Action.CLEANING)
+            clean_task = Task(data=self.data, private_data_size=self.config.private_data_size,
+                              synthetic_data_size=synthetic_data_size, fds=self.fds, action=Action.CLEANING)
             marginals_task = self.clean_data.remote(self, clean_task)
             synthesizing_task = self.get_marginals.remote(self, marginals_task)
             for _ in range(self.config.generations_repeats):
