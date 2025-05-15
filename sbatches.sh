51070326f657b5668c7a71f1381770e4dfa1f792
Itay <test@test.com>
2025-05-15 17:56:43 +0300

    Let's try

--- sbatches.sh
+++ sbatches.sh
@@ -6,11 +6,11 @@ sbatch ./run.sh mst_private.json
 sbatch ./run.sh mst_synthetic.json
 sbatch ./run.sh mst_constraints.json
 
-sbatch ./run.sh patectgan_ilp_synthetic.json
-sbatch ./run.sh patectgan_ilp_constraints.json
+sbatch ./run.sh patectgan_greedy_synthetic.json
+sbatch ./run.sh patectgan_greedy_constraints.json
 
-sbatch ./run.sh mst_ilp_synthetic.json
-sbatch ./run.sh mst_ilp_constraints.json
+sbatch ./run.sh mst_greedy_synthetic.json
+sbatch ./run.sh mst_greedy_constraints.json
 
 sbatch ./run.sh patectgan_vertex_cover_synthetic.json
 sbatch ./run.sh patectgan_vertex_cover_constraints.json
@@ -18,8 +18,8 @@ sbatch ./run.sh patectgan_vertex_cover_constraints.json
 sbatch ./run.sh mst_vertex_cover_synthetic.json
 sbatch ./run.sh mst_vertex_cover_constraints.json
 
-sbatch ./run.sh patectgan_greedy_synthetic.json
-sbatch ./run.sh patectgan_greedy_constraints.json
+sbatch ./run.sh patectgan_ilp_synthetic.json
+sbatch ./run.sh patectgan_ilp_constraints.json
 
-sbatch ./run.sh mst_greedy_synthetic.json
-sbatch ./run.sh mst_greedy_constraints.json
+sbatch ./run.sh mst_ilp_synthetic.json
+sbatch ./run.sh mst_ilp_constraints.json
