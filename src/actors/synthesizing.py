1dba19f86a123419cab5a9d838e327f7b0863ca8
Itay <test@test.com>
2025-05-16 12:50:21 +0300

    Got it

--- src/actors/synthesizing.py
+++ src/actors/synthesizing.py
@@ -11,11 +11,11 @@ def generate_synthetic_data(data: DataFrame, training_epsilon: float, model_name
                             unique_values_threshold: float, model_extra_data: dict[str, Any],
                             sample_size: int) -> DataFrame:
     MSTSynthesizer.compress_domain = smart_noise_fixes.mst_compress_domain
-    preprocess_epsilon = 1
-    eps = training_epsilon + preprocess_epsilon
-    model = Synthesizer.create(synth=model_name, epsilon=eps, verbose=True, **model_extra_data)
     continuous_columns = data.columns[data.nunique() > unique_values_threshold].tolist()
     categorical_columns = data.columns.drop(continuous_columns).tolist()
+    preprocess_epsilon = 1
+    eps = training_epsilon + preprocess_epsilon if len(continuous_columns) != 0 else training_epsilon
+    model = Synthesizer.create(synth=model_name, epsilon=eps, verbose=True, **model_extra_data)
     model.fit(data, categorical_columns=categorical_columns, continuous_columns=continuous_columns,
               preprocessor_eps=preprocess_epsilon)
     return model.sample(sample_size)
