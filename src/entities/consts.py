8549d18ebb182ecad317be0e8c89f1bcf3796e01
Itay <test@test.com>
2025-05-31 12:47:06 +0300

    Wrong path

--- src/entities/consts.py
+++ src/entities/consts.py
@@ -1,6 +1,6 @@
 import os
 
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DATASETS_DIR = os.path.join(ROOT_DIR, 'datasets')
 TASKS_CONFIGURATION_DIR = os.path.join(ROOT_DIR, 'tasks_configuration')
 LICENSES_DIR = os.path.join(ROOT_DIR, 'licenses')
